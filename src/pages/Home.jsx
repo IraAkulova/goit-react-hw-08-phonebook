09b3d0903b54791c36ee26120f1cfa0cbac25115
Ira Akulova <ira.akulova2019@gmail.com>
2023-04-27 16:43:52 +0300

    updated home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,31 @@
+const styles = {
+  container: {
+    minHeight: 'calc(100vh - 50px)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 500,
+    fontSize: 48,
+    textAlign: 'center',
+  },
+  text: {
+    fontWeight: 400,
+    fontSize: 24,
+    textAlign: 'center',
+  },
+};
+
 const Home = () => {
   return (
-    <div>
-      <h1
-        style={{
-          color: 'blue',
-        }}
-      >
-        Welcome! Please, register or login to continue.
-      </h1>
+    <div style={styles.container}>
+      <h1 style={styles.title}>Welcome, Dear friend!</h1>
+      <p style={styles.text}>
+        This service will help you quickly and conveniently create your contact
+        book. Your contact book will have a simple interface and the most
+        necessary functions: you will be able to create a contact, delete it or
+        search by name in the contact list.
+      </p>
     </div>
   );
 };
